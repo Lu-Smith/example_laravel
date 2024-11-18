0dd5667e5973100109d4bd44b469438097aa7bee
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-18 15:16:25 +0000

    Added tailwind styling

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,1 +1,1 @@
-<a {{ $attributes }}>{{ $slot }}</a>
+<a {{ $attributes }} class="rounded-md px-3 py-2 font-medium" >{{ $slot }}</a>
--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+  <x-slot:heading>
+    Info Page
+  </x-slot:heading>
   <h1>Hello from my Info Page</h1>
 </x-layout>
