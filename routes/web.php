1262dbc97b1fa09fc2d5c9e92212bfbc4e5b43d6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-19 10:19:45 +0000

    Created Job.php to place class Job from web.php

--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
-
-class Job {
-
-    public static function all(): array 
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50.000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$35.000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Nurse',
-                'salary' => '$30.000',
-            ]
-        ];
-    }
-}
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
