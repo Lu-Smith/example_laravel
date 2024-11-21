f04c52056f1e359d04616b9c3f66b35c8476df01
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-21 11:48:17 +0000

    Updated tables for Post and Comment

--- database/migrations/2024_11_21_110447_create_comments_table.php
+++ database/migrations/2024_11_21_110447_create_comments_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->string('comment');
+            $table->string('date');
             $table->timestamps();
         });
     }
