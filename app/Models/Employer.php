725620a992e91b75c1ae2c4723a705dab012418b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-21 11:00:06 +0000

    Created jobs() relationship with Employer

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+use Illuminate\Database\Eloquent\Model;
 
 class Employer extends Model
 {
+    /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    public function jobs() {
+        return $this->hasMany(Job::class);
+    }
 }
